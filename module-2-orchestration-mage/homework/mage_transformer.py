290595dbe43bc18bcbeddfa60144b23f6dec6009
venren <125124552+venren@users.noreply.github.com>
2024-02-03 13:20:15 +0000

    Homework updated.

--- module-2-orchestration-mage/homework/mage_transformer.py
+++ module-2-orchestration-mage/homework/mage_transformer.py
@@ -6,7 +6,7 @@ if 'test' not in globals():
 
 @transformer
 def transform(data, *args, **kwargs):
-    data = data[data.passenger_count>0]
+    data = data[(data.passenger_count>0) & (data.trip_distance>0)]
     data['lpep_pickup_date'] = data['lpep_pickup_datetime'].dt.date
     data['lpep_dropoff_date'] = data['lpep_dropoff_datetime'].dt.date
     data.rename(columns={'VendorID': 'vendor_id', 
@@ -20,4 +20,10 @@ def test_output(output, *args) -> None:
     """
     Template code for testing the output of the block.
     """
+    filtered = output[output.passenger_count==0]
+    trip_distance_filtered = output[output.trip_distance==0]
+    assert 'vendor_id' in output.columns
+    assert len(filtered)==0
+    print(len)
+    assert len(trip_distance_filtered)==0
     assert output is not None, 'The output is undefined'
