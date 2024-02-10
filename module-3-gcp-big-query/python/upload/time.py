3c04265504629be96e33877ab84330b068428449
venren <125124552+venren@users.noreply.github.com>
2024-02-10 14:16:06 +0000

    Remove time.py

--- module-3-gcp-big-query/python/upload/time.py
+++
@@ -1,9 +0,0 @@
-from time import time
-
-start = time()
-end = time()
-sleep(10)
-
-print('Inserted another batch ... %.3f second' % (end - start)) 
-print('Inserted another batch ... {:.3f} second'.format(end - start)) 
-
